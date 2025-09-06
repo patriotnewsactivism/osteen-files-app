feed29f26f0a0c19a3be1f8d98eab3a1d586fa1f
patriotnewsactivism <patriotnewsactivism@gmail.com>
2025-09-06 15:53:46 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import OsteenEvidenceHub from './App'
 import './index.css'
-
+import { HelmetProvider } from "react-helmet-async";
+// ...
+<HelmetProvider>
+  <App />
+</HelmetProvider>
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <OsteenEvidenceHub />
