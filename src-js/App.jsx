25c7b0fc8cec073dcb765c98484f799faf99bf9f
Matthew Reardon <patriotnewsactivism@proton.me>
2025-09-06 22:18:45 -0500

    Update App.jsx

--- src-js/App.jsx
+++ src-js/App.jsx
@@ -1,25 +1,13 @@
-// src-js/App.jsx
-import { Routes, Route } from "react-router-dom";
-import SiteHeader from "./components/SiteHeader.js";
-import BottomTab from "./components/BottomTab.js";
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
-import Home from "./pages/Home.js";
-import Music from "./pages/Music.js";
-import BadActors from "./pages/BadActors.js";
-import Evidence from "./pages/OsteenEvidence.js";
-
-export default function App() {
+function App() {
   return (
-    <div className="flex min-h-screen flex-col bg-gray-50">
-      <SiteHeader />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/music" element={<Music />} />
-        <Route path="/album/bad-actors" element={<BadActors />} />
-        <Route path="/evidence" element={<Evidence />} />
-        {/* add more routes here as needed */}
-      </Routes>
-      <BottomTab />
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* other routes */}
+    </Routes>
+  )
 }
+
+export default App
