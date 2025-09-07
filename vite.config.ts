e8b5e66e9c5cfa38ee3c09dbc9509a59ac37ee45
Matthew Reardon <patriotnewsactivism@proton.me>
2025-09-06 22:12:04 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'node:path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src-js'),
-    },
-  },
-});
+  esbuild: {
+    loader: 'jsx',
+    include: /src.*\.[jt]sx?$/,
+    // This allows JSX in .js files
+    exclude: []
+  }
+})
