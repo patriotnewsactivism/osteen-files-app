38bcca0bf034676d07587d0a3748aa9b0e05012b
patriotnewsactivism <patriotnewsactivism@gmail.com>
2025-09-08 21:45:19 -0500

    fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
-﻿// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-export default {
+﻿// tailwind.config.cjs
+module.exports = {
   content: [
     './index.html',
     './src/**/*.{js,jsx,ts,tsx,html}',
